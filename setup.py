097a818c445bbb0caf3105cbe0ab2bb8e53d289b
Tony Laidig <laidig@gmail.com>
2017-02-06 15:28:14 -0500

    updates

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='flaskr',
-    packages=['flaskr'],
+    name='gtfs_calendar_editor',
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask',
+        'Flask','flask-cors','mzgtfs'
     ],
     setup_requires=[
         'pytest-runner',
