216440a871635be94594b3fa738e4bbd9c408a23
willwong430 <willwong430@gmail.com>
2017-02-07 15:10:23 -0500

    test gtfs file

--- flaskr/tests/calendar_tests.py
+++ flaskr/tests/calendar_tests.py
@@ -9,7 +9,7 @@ import time
 from gtfsHandler import GtfsHandler 
 
 boroughs = ['staten_island']
-path = "../flaskr/gtfs_files/"
+path = "gtfs_files/"
 gtfs_calendar_handler = GtfsHandler()
 reg_service = '20160927'
 
