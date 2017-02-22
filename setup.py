45ba06c8edd2d41d193e4fdec2bf9f8b3b26b5f5
willwong430 <willwong430@gmail.com>
2017-02-22 16:49:07 -0500

    Remove dead code

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ setup(
     name='gtfs_calendar_editor',
     packages=find_packages(),
     include_package_data=True,
-    # install_requires=[
-    #     'Flask','flask-cors','mzgtfs'
-    # ],
     tests_require=[
         'unittest',
     ],
